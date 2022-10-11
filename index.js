b46bd637e3e8987b141b2f3d68173e3fe29a44b8
robwils <robwils.fs@gmail.com>
2022-10-11 19:03:56 -0400

    add unique ID to each line and box in grid

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 console.log("connected");
 
+// Define UI variables
+const reset = document.querySelector(".reset");
 const lines = document.querySelectorAll(".line");
-const state = [];
-console.log(lines);
+const squares = document.querySelectorAll(".square");
 
-lines.forEach((element) => {
-  state.push({ element: element });
-  element.addEventListener("click", cellClicked);
+reset.addEventListener("click", () => {
+  console.log("reset");
 });
 
-console.log(state);
+lines.forEach((element) => {
+  element.addEventListener("click", () => {
+    console.log(`${element.id} clicked`);
+  });
+});
 
-function cellClicked(event) {
-  console.log("clicked");
-}
+squares.forEach((element) => {
+  element.addEventListener("click", () => {
+    console.log(`${element.id} clicked`);
+  });
+});
+
+// create prototype box object
+// create array of box objects and export as module
+// objects should have methods to:
+// -- determine if the box is won
+// -- if box is won gray it out
+// -- who was the user who won
+
+// create array of unclicked lines?
+
+// create function to reset game
+// create a function to keep score
+// create function to determine when game is won
+// create a function to flag which player won the square
+// create a click function and add it to lines
